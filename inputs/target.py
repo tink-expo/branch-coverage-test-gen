1df74508010da3b4e35df1feed55546cabf023c3
tink-expo <whhan8485@gmail.com>
2020-06-01 22:13:10 +0900

    File structure

---
+++ inputs/target.py
@@ -0,0 +1,37 @@
+import copy
+
+def k(x):
+    pass
+
+def f(x, y):
+    if x > 10:
+        if x > 20 and y:
+            if x < 30:
+                pass
+            else:
+                if x > 40:
+                    pass
+        else:
+            if x < 5:  # Impossible to cover True branch of this statement.
+                pass
+    if x == 60:
+        pass
+
+    if x > 70:
+        if copy.copy(True):  # Impossible to cover False branch of this statement.
+            pass
+
+def g(x):
+    for _ in range(x):
+        pass
+    else:
+        pass
+
+    while x < 5:
+        x += 1
+    else:
+        if x > 10:
+            if x < 12:
+                pass
+        else:
+            pass
